1368abb63b2ba8652ed45419e4eb2339bd49d24e
DarynaBond <>
2020-06-07 18:41:34 +0300

    adding Sorting and tests template

---
+++ Exam/Test/Test.cpp
@@ -0,0 +1,134 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include "doctest.h"
+#include <vector>
+
+TEST_CASE("Data")
+{
+    SUBCASE("Creating data")
+    {
+    }
+}
+
+TEST_CASE("List")
+{
+    SUBCASE("Creating DList")
+    {
+    }
+
+    SUBCASE("Creating СDList")
+    {
+    }
+}
+
+TEST_CASE("Hash")
+{
+
+    SUBCASE("Coalesced hashing")
+    {
+    }
+
+    SUBCASE("Hopscotch hashing")
+    {
+    }
+}
+
+TEST_CASE("Sorting")
+{
+
+    SUBCASE("Selection Sort")
+    {
+    }
+
+    SUBCASE("Heap Sort")
+    {
+    }
+
+    SUBCASE("Merge Sort")
+    {
+    }
+
+    SUBCASE("Bucket Sort")
+    {
+    }
+
+    SUBCASE("Insertion Sort")
+    {
+    }
+
+    SUBCASE("Quicksort")
+    {
+    }
+
+    SUBCASE("Radix Sort")
+    {
+    }
+}
+
+TEST_CASE("Key-value")
+{
+
+    SUBCASE("get Value by Key")
+    {
+    }
+
+    SUBCASE("change Value by Key")
+    {
+    }
+
+    SUBCASE("add Value by Key")
+    {
+    }
+
+    SUBCASE("delete Value by Key")
+    {
+    }
+
+    SUBCASE("get all Keys")
+    {
+    }
+
+    SUBCASE("get all Values")
+    {
+    }
+
+    SUBCASE("get all Key-value")
+    {
+    }
+}
+
+TEST_CASE("Set")
+{
+
+    SUBCASE("find element")
+    {
+    }
+
+    SUBCASE("add element")
+    {
+    }
+
+    SUBCASE("delete element")
+    {
+    }
+}
+
+TEST_CASE("Set operations")
+{
+
+    SUBCASE("union")
+    {
+    }
+
+    SUBCASE("intersection")
+    {
+    }
+
+    SUBCASE("difference")
+    {
+    }
+
+    SUBCASE("symmetric_difference")
+    {
+    }
+}
