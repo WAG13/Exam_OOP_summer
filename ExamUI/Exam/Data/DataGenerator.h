b29715f2339976b570e1e54d69bb1badcee3e1f0
ozzwoy <dima.pashenko.200081@gmail.com>
2020-06-10 21:52:38 +0300

    Solved Linker Error

--- ExamUI/Exam/Data/DataGenerator.h
+++ ExamUI/Exam/Data/DataGenerator.h
@@ -7,6 +7,28 @@
 
 using namespace std;
 
+DateTime getRandomDate(DateTime min, DateTime max) {
+    if (min>max) std::swap(min, max);
+    unsigned int year = Random::getRandomInt(min.getYear(), max.getYear());
+    unsigned int month = Random::getRandomInt(1, 12);
+    unsigned int day = Random::getRandomInt(1, 31);
+    unsigned int hour = Random::getRandomInt(0, 23);
+    unsigned int minute = Random::getRandomInt(0, 59);
+    unsigned int sec = Random::getRandomInt(0, 59);
+    DateTime result(year, month, day, hour, minute, sec);
+    if ((result < min) || (result > max)) result = getRandomDate(min, max);
+    return result;
+}
+
+template<>
+vector<DateTime> Random::getRandomVector(DateTime min, DateTime max, size_t size) {
+    vector<DateTime> result;
+    for (size_t i = 0; i < size; i++) {
+        result.push_back(getRandomDate(min, max));
+    }
+    return result;
+}
+
 template<typename T>
 class DataGenerator
 {
--- ExamUI/Exam/Data/Random.h
+++ ExamUI/Exam/Data/Random.h
@@ -2,7 +2,6 @@
 
 #include <random>
 #include <vector>
-#include "DateTime.h"
 
 using namespace std;
 
@@ -15,33 +14,11 @@ namespace Random {
         return dis(gen);
     }
 
-    DateTime getRandomDate(DateTime min, DateTime max) {
-        if (min>max) std::swap(min, max);
-        unsigned int year = getRandomInt(min.getYear(), max.getYear());
-        unsigned int month = getRandomInt(1, 12);
-        unsigned int day = getRandomInt(1, 31);
-        unsigned int hour = getRandomInt(0, 23);
-        unsigned int minute = getRandomInt(0, 59);
-        unsigned int sec = getRandomInt(0, 59);
-        DateTime result(year, month, day, hour, minute, sec);
-        if ((result < min) || (result > max)) result = getRandomDate(min, max);
-        return result;
-    }
-
     template<typename T>
     vector<T> getRandomVector(T min, T max, size_t size) {
         vector<T> result;
         for (size_t i = 0; i < size; i++) {
             result.push_back(getRandomInt(min, max));
-        }
-        return result;
-    }
-
-    template<>
-    vector<DateTime> getRandomVector(DateTime min, DateTime max, size_t size) {
-        vector<DateTime> result;
-        for (size_t i = 0; i < size; i++) {
-            result.push_back(getRandomDate(min, max));
         }
         return result;
     }
